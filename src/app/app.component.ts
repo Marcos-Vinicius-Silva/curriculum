5940ed9c6d27ee6b9af21899af5c5e73a846f052
Marcos <marcos1998souza@hotmail.com>
2025-02-05 15:57:09 -0300

    fix webcomponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LeftContainerComponent } from './left-container/left-container.component';
 import { MainControlComponent } from './main-control/main-control.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LeftContainerComponent, MainControlComponent],
+  imports: [LeftContainerComponent, MainControlComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
